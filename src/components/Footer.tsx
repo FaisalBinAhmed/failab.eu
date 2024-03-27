3b8e7591de74196b87198bee483012c16ee838fa
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-03-27 17:12:20 +0100

    footer ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,29 +8,31 @@ const Footer = () => {
 	}
 
 	return (
-		<div className="flex flex-col gap-4 bg-[#121212] h-96 w-full items-center text-gray-400">
-			<div className="flex flex-row gap-4 pt-8">
-				<img
-					onClick={() => openLink("https://twitter.com/failab_eu")}
-					src="/assets/twitter.svg"
-					className="cursor-pointer"
-				/>
-				<img
-					onClick={() => openLink("https://paypal.me/FaisalDE")}
-					src="/assets/paypal.svg"
-					className="cursor-pointer"
-				/>
-			</div>
-			<div className="flex gap-2">
-				<Link href="/about">About</Link>
-				<Link href="/contact">Contact</Link>
-			</div>
-			<div className="flex flex-col md:flex-row md:justify-between items-center w-full p-8">
-				<div className=" text-center md:text-left">
+		<div className="flex flex-col gap-4 bg-[#121212] text-gray-400">
+			<div className="flex flex-col md:flex-row md:justify-between items-center h-96 w-full p-8">
+				<div className="flex flex-col items-center md:items-start text-center md:text-left">
+					<div className="flex flex-row gap-2">
+						<img
+							onClick={() => openLink("https://twitter.com/failab_eu")}
+							src="/assets/twitter.svg"
+							className="cursor-pointer"
+						/>
+						<img
+							onClick={() => openLink("https://paypal.me/FaisalDE")}
+							src="/assets/paypal.svg"
+							className="cursor-pointer"
+						/>
+					</div>
+					<div className="flex gap-2 mb-16">
+						<Link href="/about">About</Link>
+						<Link href="/contact">Contact</Link>
+					</div>
 					<p>© Failab {date}</p>
 					<p>Made with ♥ in Munich</p>
 				</div>
-				<img src="/assets/munich.png" />
+				<div className="flex flex-col justify-end h-full">
+					<img className="mb-8" src="/assets/munich.png" />
+				</div>
 			</div>
 		</div>
 	);
