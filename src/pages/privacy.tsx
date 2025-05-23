18228c9dc9b02825a986d8abab5bdd4d9c773f31
Faisal Bin Ahmed <faisalbin@outlook.com>
2025-05-24 03:23:33 +0600

    privacy

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,20 @@
+export default function PrivacyPage() {
+	return (
+		<div className="flex flex-col gap-4 p-8 max-w-4xl h-lvh">
+			<h1 className="text-lg font-bold">
+				General Privacy Policy for Failab apps
+			</h1>
+			<p className="">
+				Failab apps are committed to protecting your privacy. Our apps do not
+				collect any data or personal information. User preference for the app is
+				stored within the device itself and does not leave the device. We do not
+				use any third-party services that may collect information used to
+				identify you.
+			</p>
+			<div>
+				<p className="text-m font-bold">Last Updated:</p>
+				<p>24th May 2025</p>
+			</div>
+		</div>
+	);
+}
