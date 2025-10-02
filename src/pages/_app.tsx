2e45c41141e208ee01c48f501af1f85410375580
Faisal Bin Ahmed <faisalbin@outlook.com>
2025-10-02 17:49:34 +0200

    remove vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
 import Footer from "@/components/Footer";
 import { NavBar } from "@/components/NavBar";
 import "@/styles/globals.css";
@@ -7,7 +6,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Analytics />
 			<NavBar />
 			<Component {...pageProps} />
 			<Footer />
